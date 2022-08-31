b5cd67ffaf6b45f200bdcd023020f1459345befb
Lou Franco <lfranco@greenwave-solutions.com>
2022-08-30 21:21:44 -0400

    Write section titles

--- pageomat/pages/template/horizontal_sections.py
+++ pageomat/pages/template/horizontal_sections.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from math import floor
 from pageomat.config import config_page_attribute
 from pageomat.pages.color_utils import hex2blue, hex2green, hex2red
@@ -26,25 +27,59 @@ class HorizontalSectionsTemplate(TemplatePage):
         grid_snap = config_page_attribute(config, page, "grid-snap", 0)
         spacing = self.grid_snap_value(grid_snap, height / section_count)
 
+        section_title = config_page_attribute(config, page, "section-title", None)
+
+        section_font = config_page_attribute(config, page, "section-title-font", {"family": "Helvetica", "size": 10})
+        pdf.set_font(section_font["family"], size=section_font["size"])
+        section_color = config_page_attribute(config, page, "section-title-color", "#000")
+        pdf.set_text_color(hex2red(section_color), hex2green(section_color), hex2blue(section_color))
+
+        section_day_of_year = config_page_attribute(config, page, "section-day-of-year", None)
+        section_date_format = config_page_attribute(config, page, "section-date-format", "%y-%MM-%dd")
+        year = config_page_attribute(config, page, "year", 2023)
+
+        def write_section_title():
+            nonlocal section_day_of_year
+            nonlocal section
+            title = section_title
+            if section_title is not None:
+                if section_day_of_year is not None:
+                    doy = section_day_of_year
+                    if type(section_day_of_year) is str:
+                        vars = {"__builtins__": None, "s": section}
+                        vars.update(page["indices"])
+                        doy = eval(section_day_of_year, vars, {})
+                    date = datetime.strptime(str(year) + "-" + str(doy), "%Y-%j")
+                    date_str = datetime.strftime(date, section_date_format)
+                    title = title.replace("$section-date$", date_str)
+
+                pdf.text(10, yPos - spacing + grid_snap, title)
+
         if section_count % 2 == 0:
             # If we have an even number of sections, then the
             # center line of the page will be the middle line.
-
             y_lines = floor((section_count - 1) / 2)
-            for y in range(-y_lines, y_lines + 1):
-                pdf.line(
-                    0, center_y + spacing * y,
-                    width, center_y + spacing * y)
+            for y in range(-y_lines, y_lines + 2):
+                yPos = center_y + spacing * y
+                if y < y_lines + 1:
+                    pdf.line(0, yPos, width, yPos)
+
+                section = y + y_lines  # 0 index
+                if section < section_count:
+                    write_section_title()
         else:
             # If we have an odd number of sections, then we'll
             # try to center the middle one around the center line of the page
             # But, if the grid-snap requires it, we'll snap to the closest
             # grid line.
-            y_lines = floor((section_count - 1) / 2)
-            for y in range(-y_lines, y_lines + 1):
+            y_lines = floor(section_count / 2)
+            for y in range(-y_lines, y_lines + 2):
                 # We need to skip the 0 (no line in the center)
                 if y != 0:
                     offset = (-spacing / 2 if y > 0 else spacing / 2) + grid_snap / 2
-                    pdf.line(
-                        0, center_y + spacing * y + offset,
-                        width, center_y + spacing * y + offset)
+                    yPos = center_y + spacing * y + offset
+                    if y < y_lines + 1:
+                        pdf.line(0, yPos, width, yPos)
+
+                    section = y + y_lines if y < 0 else y + y_lines - 1  # 0 index
+                    write_section_title()
