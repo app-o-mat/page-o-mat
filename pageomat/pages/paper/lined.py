63e21c7187ea3a532461c9fe1f0eef175ce4f98d
Lou Franco <lfranco@greenwave-solutions.com>
2022-09-18 16:19:59 -0400

    Add lined paper

---
+++ pageomat/pages/paper/lined.py
@@ -0,0 +1,31 @@
+from math import floor
+from pageomat.pages.color_utils import hex2blue, hex2green, hex2red
+from pageomat.pages.page import Paper
+
+
+def make_paper():
+    return LinedPaper()
+
+
+class LinedPaper(Paper):
+
+    def render_into(self, config, paper, pdf):
+        super().render_into(config, paper, pdf)
+        width = self.page_width(config)
+        height = self.page_height(config)
+        line_width = self.paper_attribute(paper, "line-width", 0.25)
+        heading = self.paper_attribute(paper, "heading", 0)
+
+        spacing = self.paper_attribute(paper, "spacing", 5)
+
+        center_y = height / 2
+        y_lines = floor(center_y / spacing)
+
+        color = self.paper_attribute(paper, "color", "#000")
+        pdf.set_draw_color(hex2red(color), hex2green(color), hex2blue(color))
+        pdf.set_line_width(line_width)
+
+        for y in range(-y_lines, y_lines + 1):
+            y_pos = center_y + spacing * y
+            if y_pos >= heading:
+                pdf.line(0, y_pos, width, y_pos)
