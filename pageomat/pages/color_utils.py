395313afa50d3794290e50e3b9fbbb534f4240e7
Lou Franco <lfranco@greenwave-solutions.com>
2023-09-17 10:59:30 -0400

    Add a way to get alpha from a color

--- pageomat/pages/color_utils.py
+++ pageomat/pages/color_utils.py
@@ -4,8 +4,14 @@ def hex2num(hex):
 
 def hex_part(hex, part):
     if len(hex) == 3:
+        if part == 3:
+            return "FF"
         return hex[part:part + 1] + hex[part:part + 1]
     elif len(hex) == 6:
+        if part == 3:
+            return "FF"
+        return hex[part * 2: (part + 1) * 2]
+    elif len(hex) == 8:
         return hex[part * 2: (part + 1) * 2]
     else:
         raise ValueError("Invalid color: " + hex)
@@ -24,3 +30,8 @@ def hex2green(hex):
 def hex2blue(hex):
     hex = hex.removeprefix("#")
     return hex2num(hex_part(hex, 2))
+
+
+def hex2alpha(hex):
+    hex = hex.removeprefix("#")
+    return hex2num(hex_part(hex, 3))
