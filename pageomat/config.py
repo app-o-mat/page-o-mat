4de354904355a3625a4b2f4b3a17d822b76bc878
Lou Franco <lfranco@greenwave-solutions.com>
2024-12-25 20:36:18 -0500

    eval_value moved to top-level config funcs

--- pageomat/config.py
+++ pageomat/config.py
@@ -12,3 +12,18 @@ def config_attribute(config, key, default_value):
         return config[key]
     else:
         return default_value
+
+
+def eval_value(value, indices):
+    if type(value) is str:
+        vars = {"__builtins__": None}
+        if indices is not None:
+            vars.update(indices)
+        value = eval(value, vars)
+
+    return value
+
+
+def eval_value_with_key(config, page, indices, key, default_value=None):
+    value = config_page_attribute(page, config, key, default_value=default_value)
+    return eval_value(value, indices)
