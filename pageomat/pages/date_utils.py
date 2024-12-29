28cc30ad2e9c4c536206a7cb53ef58d52f708134
Lou Franco <lfranco@greenwave-solutions.com>
2024-12-29 17:01:05 -0500

    [payment] eval_with_variables refactored out of DOY formatting (to also do year)

--- pageomat/pages/date_utils.py
+++ pageomat/pages/date_utils.py
@@ -1,17 +1,30 @@
 from datetime import datetime
 
 
-def format_day_of_year(year, day_of_year, date_format, vars):
-    doy = day_of_year
-    if type(day_of_year) is str:
+def eval_with_variables(value, vars):
+    return_val = value
+    if type(value) is str:
         try:
-            doy = eval(day_of_year, vars, {})
+            return_val = eval(value, vars, {})
         except BaseException:
-            print("Error evaluating: " + day_of_year)
+            print("Error evaluating: " + value)
             print("with variables " + str(vars))
             raise
-    date = datetime.strptime(str(year) + "-" + str(doy), "%Y-%j")
-    return datetime.strftime(date, date_format)
+    return return_val
+
+
+def format_day_of_year(year, day_of_year, date_format, vars):
+    try:
+        doy = eval_with_variables(day_of_year, vars)
+        y = eval_with_variables(year, vars)
+        date = datetime.strptime(str(y) + "-" + str(doy), "%Y-%j")
+        return datetime.strftime(date, date_format)
+    except BaseException:
+        print("Error formatting day of year: " + str(y) + " " + str(doy) + " " + str(date_format))
+        print("day_of_year: " + str(day_of_year))
+        print("year: " + str(year))
+        print("with variables " + str(vars))
+        raise
 
 
 def date_replace(title, key, year, day_of_year, date_format, vars):
